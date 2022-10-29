5f57525fc810e49c99b6406fd231d91e28ba2204
erikoleary <erik.m.oleary@gmail.com>
2022-10-29 11:02:53 -0500

    Added sample

--- Sample/Controllers/SampleController.cs
+++ Sample/Controllers/SampleController.cs
@@ -1,13 +1,25 @@
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("[controller]")]
 public class SampleController : ControllerBase
 {
-    [HttpGet]
+    [HttpGet(Name = "Test API Key")]
+    [Authorize(AuthenticationSchemes = "x-api-key")]
     public IActionResult Get()
     {
         return Ok("Hello World!");
+    }
+
+    [HttpGet("GetNewKey", Name = "Unprotected")]
+    public string GenerateApiKey(
+        [FromServices] DataProtectedAPIKeyGenerator generator,
+        int validMinutes = 5)
+    {
+        var key = new SampleDataProtectionKey();
+
+        return generator.ProtectKey(key, TimeSpan.FromMinutes(validMinutes));
     }
 }
