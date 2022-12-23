39a0fb736451c3375e16bb56f1986d1ba1099d7e
Erik OLeary <erik.m.oleary@gmail.com>
2022-12-23 12:23:52 -0600

    Create artifacts, increment version

--- build.ps1
+++ build.ps1
@@ -1,17 +1,22 @@
 Param (
-    [Parameter(Mandatory=$true)]
     [string] $NugetKey,
     [switch] $Publish
 )
 
 dotnet publish src/Wivuu.AspNetCore.APIKey -c Release
 
-if ($Publish -and $NugetKey) {
-    # Find first nupkg file in directory
-    $nupkg = Get-ChildItem -Path "src/Wivuu.AspNetCore.APIKey/bin/Release" -Filter *.nupkg | Select-Object -First 1
+# Find first nupkg file in directory
+$nupkg = Get-ChildItem -Path "src/Wivuu.AspNetCore.APIKey/bin/Release" -Filter *.nupkg | Select-Object -First 1
 
+if ($Publish -and $NugetKey) {
     dotnet nuget push `
         $nupkg `
         --api-key $NugetKey `
         --source https://api.nuget.org/v3/index.json
 }
+
+# Copy to artifacts
+New-Item -ItemType Directory -Path ./artifacts -Force
+Copy-Item $nupkg -Destination ./artifacts/
+
+return $nupkg
